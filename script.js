1ff07fb0ed90b8d1177eb832effbf430fa350b6d
Semehal Hailu Woldeslassie <semehal.hailu@aastustudent.edu.et>
2025-03-16 18:31:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var nav = document.getElementById("nav");
+        function showmenu() {
+            nav.style.right = "0";
+            nav.style.display = "flex";
+        }
+        function hidemenu() {
+            nav.style.right = "-120px";
+            nav.style.display = "none";
+        }
